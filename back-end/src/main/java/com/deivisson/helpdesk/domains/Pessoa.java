eeb239a33e1eb5a4afe40e32c5abfd977731b625
Deivisson Santos <deivissonsantos@hotmail.com>
2021-11-17 11:24:57 -0300

    implementado a classe abstrata pessoa

---
+++ back-end/src/main/java/com/deivisson/helpdesk/domains/Pessoa.java
@@ -0,0 +1,46 @@
+package com.deivisson.helpdesk.domains;
+
+import com.deivisson.helpdesk.enums.Perfil;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Data
+public abstract class Pessoa {
+
+    protected Integer id;
+    protected String nome;
+    protected String cpf;
+    protected String email;
+    protected String senha;
+    protected Set<Integer> perfis = new HashSet<>();
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public Pessoa() {
+        super();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Pessoa(Integer id, String nome, String cpf, String email, String senha) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfis.add(perfil.getCodigo());
+    }
+
+
+}
