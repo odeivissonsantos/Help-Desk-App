fdfc9c8293caeefb9c37b166c1ab81b8da38cb1a
Deivisson Santos <deivissonsantos@hotmail.com>
2021-11-18 16:34:30 -0300

    implementado a busca por id

---
+++ back-end/src/main/java/com/deivisson/helpdesk/services/TecnicoService.java
@@ -0,0 +1,20 @@
+package com.deivisson.helpdesk.services;
+
+import com.deivisson.helpdesk.domains.Tecnico;
+import com.deivisson.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    public Tecnico findById(Integer id) {
+        Optional<Tecnico> obj = tecnicoRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
